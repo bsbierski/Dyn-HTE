75327ee743593d3f01b869bc4849519015e0e852
Björn Sbierski <b.sbierski@web.de>
2025-04-17 17:04:52 +0200

    All-To-All_N4: test successful for S=1

--- CaseStudy/All-To-All_N4/All-To-All_N4.jl
+++ CaseStudy/All-To-All_N4/All-To-All_N4.jl
@@ -1,26 +1,32 @@
 using JLD2,DelimitedFiles
 include("../../plotConventions.jl")
-include("../../Embedding.jl")
 include("../../LatticeGraphs.jl")
+include("../../Embedding.jl")
 include("../../ConvenienceFunctions.jl") 
 
 ### maximum expansion order and spin-length
-n_max = 11
-spin_string = "S1" #"S1half" #"S3half"
-
-### load list of unique graphs
-gG_vec_unique = give_unique_gG_vec(n_max);
-
-### load dictionaries of all lower orders C_Dict_vec 
-C_Dict_vec = Vector{Vector{Vector{Rational{Int128}}}}(undef,n_max+1);
-for ord = 0:n_max
-    C_Dict_vec[ord+1]  = load_object("GraphEvaluations/Spin_"*spin_string*"/C_"*string(ord)*".jld2")
-end 
+n_max = 12
+spin_length = 1
 
 ### define lattice for embedding
-LatGraph = complete_graph(4)
-display(graphplot(LatGraph,names=1:nv(LatGraph),markersize=0.11,fontsize=4,nodeshape=:rect,curves=false))
+Lgraph = complete_graph(4)
+#display(graphplot(LatGraph,names=1:nv(LatGraph),markersize=0.11,fontsize=4,nodeshape=:rect,curves=false))
 
-### compute all correlations in the lattice (or load them)
-c11 = Calculate_Correlator_fast(LatGraph,1,1,n_max,gG_vec_unique,C_Dict_vec)
-c12 = Calculate_Correlator_fast(LatGraph,1,2,n_max,gG_vec_unique,C_Dict_vec)
+### load graphG evaluations 
+hte_graphs = load_dyn_hte_graphs(spin_length,n_max);
+
+if true ### plot the C_n vs graphG index
+    plt=plot(yscale=:log10,xlabel="graph index",ylabel="denom C_n",title="spin length $spin_length")
+    for n in n_max:-1:8
+        tmp = hte_graphs.c_dict[n+1]
+        #tmp = load_object("GraphEvaluations/Spin_"*spin_string*"/Vac_"*string(n)*".jld2")
+        plot!(plt,[abs(denominator(c[1]))+1 for c in tmp],label="n=$n")
+    end
+    display(plt)
+    #savefig(plt,"C_n_"*spin_string*".png")
+end
+
+### compute all correlations in the lattice 
+c_iipDyn_mat = get_c_iipDyn_mat(Lgraph,[1],hte_graphs)
+c_iipDyn_mat[1,1]
+c_iipDyn_mat[2,1]
