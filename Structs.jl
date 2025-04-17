460b9857e07d9ec3b0f05bb689c0f3e3d354169a
Björn Sbierski <b.sbierski@web.de>
2025-04-17 17:03:23 +0200

    removed functions/structs only used for graph evaluation

--- Structs.jl
+++ Structs.jl
@@ -1,10 +1,6 @@
 using Graphs, SimpleWeightedGraphs, Parameters
 
-
-
 #from SpinMC.jl 
-
-
 struct InteractionMatrix
     m11::Float64
     m12::Float64
@@ -16,8 +12,6 @@ struct InteractionMatrix
     m32::Float64
     m33::Float64
 end
-
-
 
 #### UnitCell.jl
 struct UnitCell{D}
@@ -50,7 +44,6 @@ mutable struct Lattice{D,N}
 end
 
 
-
 ###### graph structures, all Graphs are: connected (and thus free of vertices without any edges)
 struct Graph ### connected vacuum graph
     g::SimpleWeightedGraph{Int64, Int64}
@@ -72,13 +65,6 @@ mutable struct GraphG ### V-connected graphs for correlator G with TWO external 
         new(g,jjp)
     end
 end
-
-@with_kw mutable struct VconSub ### single subtraction to evaluate V-connected GraphG
-    factor::Int = 1
-    gG_sub::Tuple{Int,Int}
-    g_sub_vec::Vector{Tuple{Int,Int}}
-end
-
 
 mutable struct gG_properties
     order::Int
@@ -119,7 +105,6 @@ mutable struct unique_Graphs
 
 end
 
-
 mutable struct Dyn_HTE_Graphs
     S :: Rational{Int}    #Spin-Length
     unique_graphs ::unique_Graphs #a dictionary ordering all graphs into equivalence classes 
@@ -132,7 +117,6 @@ mutable struct Dyn_HTE_Lattice
     graph::SimpleGraph
     basis_positions::Vector{<:Int}
 end
-
 
 mutable struct unique_Graph_precalc
     ref_graph ::GraphG
@@ -155,66 +139,4 @@ mutable struct unique_Graphs_precalc
 
 end
 
-struct GraphGinfo
-    gG::GraphG                      # graphG
-    jjp::Tuple{Int8,Int8}           # jjp
-    num_v::Int8                     # number of vertices
-    n::Int8                         # order of graph = number of all edges
-    m::Int8                         # different edges
-    edge_vec::Vector{Vector{Int8}}  # all different edges in their order as in gG
-    mult_vec::Vector{Int8}          # multiplicity of edges
-    v_size::Vector{Int8}            # lengths of operator string for each vertex
-    praw_vec::Vector{Int8}          # raw ordering vec of edge operators [1,2,2,3]
-    
-    function GraphGinfo(gG::GraphG)
-        """ from gG::GraphG to GraphGinfo """
-        num_v = nv(gG.g)        
-        n = totalEdges(gG.g)    
-        m = length(edges(gG.g))
-
-        ### operator ordering info
-        edge_vec = Vector{Int}[]    
-        mult_vec = Int[]            
-        praw_vec = Int[]            # m operators ordered as the numbers in p_vec: 1=Ve1, 2=Ve2,...
-        
-        multi_edge_vec = collect(edges(gG.g))
-        for pos in eachindex(multi_edge_vec)
-            e = multi_edge_vec[pos]
-            push!(edge_vec, [src(e),dst(e)])
-            push!(mult_vec, get_weight(gG.g,src(e),dst(e)))
-            for _ in 1:get_weight(gG.g,src(e),dst(e))
-                push!(praw_vec,pos)
-            end
-        end
-        
-        ### find lengths of all vertex operator strings
-        v_size=zeros(Int,num_v)
-        for e_pos in eachindex(edge_vec)
-            e = 1*edge_vec[e_pos]
-            v_size[e[1]] += mult_vec[e_pos]
-            v_size[e[2]] += mult_vec[e_pos]
-        end
-        v_size[gG.jjp[1]] +=1
-        v_size[gG.jjp[2]] +=1
-
-        ### build struct
-        new(gG,(gG.jjp[1],gG.jjp[2]),num_v,n,m,edge_vec,mult_vec,v_size,praw_vec)
-    end
-end
-
-struct Struct_γ
-    """ structure for all flavors to sum over """
-    meγ::Vector{Vector{Int8}}   # multi-edge-operator resolved flavors, e.g. [[1,-1],[0],[1,0,-1]]
-    vγ::Vector{Vector{Int8}}    # vertex resolved flavors (same as in Config_pγv, but only in initial order)
-    Πx_fac::Int8                # multiplicity under Πx
-end
-
-mutable struct Config_pγv
-    """ struct to keep track of operator permutations, edge flavors, γ-vecs for vertices, and their trace-evals """
-    vγ::Vector{Vector{Int8}}    # γ-vecs for vertices
-    pv::Vector{Vector{Int8}}    # vertex resolved operator order [3,5,8,9] with 0 the external vertex 
-    vtr::Vector{Int}            # traces for vγ
-    pos_ext::Vector{Int8}       # position of the external operators in vertex string order
-    prod::Int                   # full trace
-end
 
