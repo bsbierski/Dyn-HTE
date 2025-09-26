1b468a6a436c7659d8c1e2939ca25fd138c2cf97
Björn Sbierski <b.sbierski@web.de>
2025-09-26 16:12:01 +0200

    Chain example updated

--- examples/Chain/Application_Chain_S1.jl
+++ examples/Chain/Application_Chain_S1.jl
@@ -1,20 +1,11 @@
 ######### Dyn-HTE for chain #########
 using JLD2,DelimitedFiles, HDF5, LaTeXStrings,Polynomials,Graphs
 using DynHTE
-
-
 include("../plotConventions.jl")
-
-#= path_DynHTSE="../../"
-include(path_DynHTSE*"LatticeGraphs.jl")
-include(path_DynHTSE*"ConvenienceFunctions.jl")
-include(path_DynHTSE*"plotConventions.jl") 
-include(path_DynHTSE*"Embedding.jl") =#
-
 
 #DEFINE THE SYSTEM
 n_max = 12                   #max order in perturbation theory (currently 12 are available)
-spin_length = 1/2            #spin length (currently only S=1/2,1 are available)
+spin_length = 1.0            #spin length (currently only S=1/2,1 are available)
 #load graphs
 hte_graphs = load_dyn_hte_graphs(spin_length,n_max)
 #define the lattice geometry
@@ -22,14 +13,12 @@ hte_lattice = getLattice(n_max,"chain")
 #calculate the correlation function for all site combinations
 c_iipDyn_mat = get_c_iipDyn_mat(hte_lattice,hte_graphs)
 #plot the lattice geometry
-display(graphplot(hte_lattice.graph,names=1:nv(hte_lattice.graph),markersize=0.2,fontsize=5,curves=false))
-
-
+#graphplot(hte_lattice.graph,names=1:nv(hte_lattice.graph),markersize=0.2,fontsize=5,curves=false)
 
 
 #### moments and δs of the continued fraction expansion
 #(we will calculate the first four moments at fixed k)
-k= 0.2*pi            #define fixed k 
+k= 0.2*π            #define fixed k 
 x_vec = 0:0.01:4.5   #define temperature range of interest
 #Fourier transform the correlation functions at k
 c_kDyn_mat = get_c_k([(k,0.0)],c_iipDyn_mat,hte_lattice)[1]
@@ -51,25 +40,22 @@ m_vec_times_x_extrapolated_u_pade1 = []
 m_vec_times_x_extrapolated_u_pade2 = []
 for m_idx=1:length(m_vec)-2
     push!(m_vec_times_x_extrapolated_u_pade1, extrapolate_series(m_vec_times_x[m_idx],"u_pade",(8-m_idx,7-m_idx,f)))
-    push!(m_vec_times_x_extrapolated_u_pade2, extrapolate_series(m_vec_times_x[m_idx],"u_pade",(7-m_idx,8-m_idx,f)))
+    push!(m_vec_times_x_extrapolated_u_pade2, extrapolate_series(m_vec_times_x[m_idx],"u_pade",(7-m_idx,7-m_idx,f)))
 end
 
 #plot the moments 
-plt_m = Plots.plot([0],[0],label="",xlabel=L"x",ylabel=L"x \cdot m_{\mathbf{k},r}(x)/m_{\mathbf{k},r}(0)",title="moments at k="*string(k/pi)*"π",legend=:topleft,xlim=(-0.2,4.5),size=(1.5*aps_width,aps_width))
+plt_m = Plots.plot([0],[0],label="",xlabel=L"x",ylabel=L"x \cdot m_{\mathbf{k},r}(x)/m_{\mathbf{k},r}(0)",title="moments at k="*string(k/pi)*"π",legend=:topleft,xlim=(-0.2,4.5),ylim=(-0.1,4.5),size=(1.5*aps_width,aps_width))
 Plots.plot!(plt_m,[0],[0],label="x bare",color = "grey",linestyle = linestyle_vec[1],linewidth=0.4)
 Plots.plot!(plt_m,[0],[0],label="u Padé [7-r,6-r]",color = "grey",linestyle = linestyle_vec[2],alpha =0.5)
-Plots.plot!(plt_m,[0],[0],label="u Padé [6-r,7-r]",color = "grey",linestyle = linestyle_vec[3])
+Plots.plot!(plt_m,[0],[0],label="u Padé [6-r,6-r]",color = "grey",linestyle = linestyle_vec[3])
 Plots.plot!(plt_m,[0],[0],label="Padé [6-r,6-r]",color = "grey",linestyle = linestyle_vec[4])
 for i=1:4
     Plots.plot!(plt_m,[0],[0],label="r="*string(i-1),color = color_vec[i])
-    Plots.plot!(plt_m,x_vec[1:180],m_vec_times_x[i].(x_vec[1:180])./(m_vec[i](0)),label = "",alpha= 0.7,color = color_vec[i],linestyle = linestyle_vec[1],linewidth=0.5)
+    Plots.plot!(plt_m,x_vec[1:100],m_vec_times_x[i].(x_vec[1:100])./(m_vec[i](0)),label = "",alpha= 0.7,color = color_vec[i],linestyle = linestyle_vec[1],linewidth=0.5)
     Plots.plot!(plt_m,x_vec,m_vec_times_x_extrapolated_u_pade1[i].(tanh.(f.*x_vec))./(m_vec[i](0)),label = "",alpha= 0.5,color = color_vec[i],linestyle = linestyle_vec[2],linewidth=0.5)
     Plots.plot!(plt_m,x_vec,m_vec_times_x_extrapolated_u_pade2[i].(tanh.(f.*x_vec))./(m_vec[i](0)),label = "",alpha= 1,color = color_vec[i],linestyle = linestyle_vec[3],linewidth=0.5)
     Plots.plot!(plt_m,x_vec,x_vec.*m_vec_extrapolated_pade[i].(x_vec)./(m_vec_extrapolated_pade[i](0.0001)),label = "",alpha= 1,color = color_vec[i],linestyle = linestyle_vec[4],linewidth=0.5)
 end
-
-#SHOW PLOT
-##############
 display(plt_m)
 
 
@@ -78,7 +64,6 @@ display(plt_m)
 #now calculate the δs of the continued fraction expansion 
 #(we will use the u_pade extrapolation)
 x_vec = [0.5,1.0,2.0,4.0]  #define the x values at which we calculate the deltas
-
 deltas_at_x =[]
 for x in x_vec
     δ_vec,r_vec = fromMomentsToδ([m(tanh(f*x))/x for m in m_vec_times_x_extrapolated_u_pade1])
@@ -90,18 +75,13 @@ plt_δ = Plots.plot([0],[0],title="δs using Padé with substitution on moments"
 for (i,x) in enumerate(x_vec)
     Plots.scatter!(plt_δ,0:3,deltas_at_x[i][1:4],label = "x="*string(x),color = thermalCol4_vec[i],markersize=7)
 end
-
-#SHOW PLOT
-##############
 display(plt_δ)
-
-
 
 
 #one can now extrapolate the deltas to infinity via
 deltas_at_x_ext = []
 for x_idx = 1:length(x_vec)
-    δ_vec_ext = extrapolate_δvec(deltas_at_x[x_idx],3,3,1000,false)
+    δ_vec_ext = extrapolate_δvec(deltas_at_x[x_idx],2,3,1000,false)
     push!(deltas_at_x_ext,δ_vec_ext)
 end
 
@@ -109,47 +89,37 @@ end
 for (i,x) in enumerate(x_vec)
     Plots.scatter!(plt_δ,4:10,deltas_at_x_ext[i][5:11],label = "",title="δs extrapolated",color = thermalCol4_vec[i],markersize=4,alpha=0.5)
 end
-
-#SHOW PLOT
-##############
 display(plt_δ)
 
 
+# use the deltas to calculate the spin structure factor (still at fixed k)
+w_vec = -3:0.01:5  #define w
+x_idx = 4         #choose temperature index (relativ to x_vec = [0.5,1.0,2.0,4.0]) 
 
-
-#we can use the deltas to calculate the spin structure factor (still at fixed k)
-w_vec = -3:0.01:3  #define w
-x_idx = 3          #choose temperature index (relativ to x_vec = [0.5,1.0,2.0,4.0]) 
-η = 0.01           #the imaginary part after analytic continuation (η ->0)
 #now calculate the DSF 
-DSF = [JS(deltas_at_x_ext[x_idx] ,x_vec[x_idx],w,η) for w in w_vec]
+extrap_params = get_extrapolation_params(deltas_at_x_ext[x_idx][1:4],2,3,false)
+DSF = [JSwithTerminator(deltas_at_x_ext[x_idx][1:4], x_vec[x_idx], w, extrap_params) for w in w_vec]
 
-plt_dsf = Plots.plot(w_vec,DSF,label="",title="DSF at k="*string(k/pi)*"π and x="*string(x_vec[x_idx]),xlabel=L"\omega/J",ylabel=L"JS(k,\omega)",size = (1.5*aps_width,aps_width))
-
-#SHOW PLOT
-##############
+Plots.plot(w_vec,DSF,label="with terminator",title="DSF at k="*string(k/pi)*"π and x="*string(x_vec[x_idx]),xlabel=L"\omega/J",ylabel=L"JS(k,\omega)",size = (1.5*aps_width,aps_width))
+plt_dsf = Plots.plot!(w_vec,DSF,label="r_max = 1000",linestyle = :dash)
 display(plt_dsf)
-
-
 
 
 ###SPIN STRUCTURE FACTOR HEATMAPS
 using CairoMakie
-x = 4.0                #define temperature (x=J/T)
-k_step_size = 1/41     #define k step size (in 1/π)
-w_step_size = 0.025    #define ω step size (in 1/J)
+x = 4.0           #define temperature (x=J/T)
+k_step_size = 1/74    #define k step size (in 1/π)
+w_step_size = 0.01    #define ω step size (in 1/J)
 #define k and ω vectors 
 k_vec = vcat(vcat((0.0001,0.0),[(k*pi,0.0) for k in 0:k_step_size:2][2:end-1] ),(1.999*pi,0.0))#[(k,0.0) for k in 0.01:0.0039*2.4:(2*π-0.01)]
 w_vec = collect(-3:w_step_size:3)
 
 #calculate the spin structure factor for the given k and ω 
-JSkw_mat = get_JSkw_mat("u_pade",x,k_vec,w_vec,c_iipDyn_mat,hte_lattice,r_min=3,r_max=3,r_ext=1000,f=0.48)
+@time JSkw_mat = get_JSkw_mat("u_pade",x,k_vec,w_vec,c_iipDyn_mat,hte_lattice,r_min=3,r_max=3,f=0.48);
 
 #plot the result
-fig = CairoMakie.Figure(size=(400,400),fontsize=20)
+fig = CairoMakie.Figure(size=(400,400),fontsize=20);
 ax=CairoMakie.Axis(fig[1,1],limits=(0,2,-3,3),xlabel=L"k/ \pi",ylabel=L"\omega/J=w",title="x="*string(x),titlesize=20,xlabelsize=20,ylabelsize=20)
-hm=CairoMakie.heatmap!(ax,[k[1]/π for k in k_vec],w_vec,JSkw_mat,colormap=:viridis,colorrange=(0.0,0.45),highclip=:white)
-
-#SHOW PLOT
-##############
+hm=CairoMakie.heatmap!(ax,[k[1]/π for k in k_vec],w_vec,JSkw_mat,colormap=:viridis,colorrange=(0.0,3.0),highclip=:white)
 display(fig)
+
--- examples/Chain/Application_Chain_S1half_Matsubara.jl
+++ examples/Chain/Application_Chain_S1half_Matsubara.jl
@@ -1,6 +1,5 @@
-using JLD2,DelimitedFiles, HDF5, LaTeXStrings,Polynomials
+using JLD2,DelimitedFiles,HDF5,Polynomials
 using DynHTE
-
 include("../plotConventions.jl")
 
 ### load graph evaluations and prepare lattice  
@@ -10,16 +9,16 @@ spin_length = 1/2
 hte_graphs = load_dyn_hte_graphs(spin_length,L);
 
 hte_lattice = getLattice(L,"chain");
-display(graphplot(hte_lattice.graph,names=1:nv(hte_lattice.graph),markersize=0.2,fontsize=7,nodeshape=:rect,curves=false))
+#display(graphplot(hte_lattice.graph,names=1:nv(hte_lattice.graph),markersize=0.2,fontsize=7,nodeshape=:rect,curves=false))
 
 ### compute all correlations in the lattice
 c_iipDyn_mat = get_c_iipDyn_mat(hte_lattice,hte_graphs)
 
 #########################################################################################
-###### Matsubara correlator at m=0,1,2 in real-space and vs QMC
+###### Matsubara correlator at m=0,1,2 in real-space, comparison to QMC
 #########################################################################################
 
-###### load QMC data at m=0,1,2 (have x=βJ=[1,2,...,8]; but one can use less) 
+###### load QMC data at m=0,1,2 (have x=[1,2,...,8]) 
 x_vec_QMC = collect(1:8)
 imax=16
 m_vec = [0,1,2]
@@ -45,20 +44,19 @@ if true
     x_vec_bare = collect(0:0.01:1.93)
     
     ### prepare panels for m=0,1,2
-    plt_m0=plot(x_vec,0*x_vec,color=:black,lw=0.8,xlims=(0,x_vec[end]),ylims=(-0.11,0.26), xformatter=:none,ylabel=L"TG_{ii^\prime}(i\nu_{m=0})",  label="",legend=:topright)
-    plt_m1=plot(x_vec,0*x_vec,color=:black,lw=0.8,xlims=(0,x_vec[end]),ylims=(-0.035,0.04),xformatter=:none,ylabel=L"TG_{ii^\prime}({i\nu_{m=1}})",label="",legend=:bottomleft)
-    plt_m2=plot(x_vec,0*x_vec,color=:black,lw=0.8,xlims=(0,x_vec[end]),ylims=(-0.01,0.02),xlabel=L"x=J/T",ylabel=L"TG_{ii^\prime}(i\nu_{m=2})",   label="",legend=:topleft)
+    plt_m0=Plots.plot(x_vec,0*x_vec,color=:black,lw=0.8,xlims=(0,x_vec[end]),ylims=(-0.12,0.26), xformatter=Returns(""),ylabel=L"TG_{ii^\prime}(i\nu_{m=0})",  label="",legend=:topright)
+    plt_m1=Plots.plot(x_vec,0*x_vec,color=:black,lw=0.8,xlims=(0,x_vec[end]),ylims=(-0.035,0.04),xformatter=Returns(""),ylabel=L"TG_{ii^\prime}({i\nu_{m=1}})",label="",legend=:bottomleft)
+    plt_m2=Plots.plot(x_vec,0*x_vec,color=:black,lw=0.8,xlims=(0,x_vec[end]),ylims=(-0.01,0.02),xlabel=L"x=J/T",ylabel=L"TG_{ii^\prime}(i\nu_{m=2})",   label="",legend=:topleft)
     plt_vec = [plt_m0,plt_m1,plt_m2]
 
     ### plot Dyn-HTE results
     for di in di_vec, (m_pos,m) in enumerate(m_vec)
-        
         p_x = get_TGiip_Matsubara_xpoly(c_iipDyn_mat,hte_lattice.basis_positions[1]+di,1,m)
 
         ### plot bare truncated series
         label=""
         if (di==0 && m==0)  label = L"\mathrm{bare \; series} \; n_{max}="*string(n_max) end    
-        plot!(plt_vec[m_pos],x_vec_bare,p_x.(x_vec_bare),label=label,color=:grey,linewidth=0.5)
+        Plots.plot!(plt_vec[m_pos],x_vec_bare,p_x.(x_vec_bare),label=label,color=:grey,linewidth=0.5)
 
         pade_vec = [[6,6],[5,5]]
 
@@ -90,18 +88,19 @@ if true
         label=""
         for  (pade_pos,pade) in enumerate(pade_vec)
             if di==0 && m==2 label="u-Padé "*string(pade)*" (f=$f)" end
-            plot!(plt_vec[m_pos],x_vec,get_pade(p_u,pade[1],pade[2]).(u_vec),color=:purple,alpha=0.6,linestyle=linestyle_vec[pade_pos],label=label)
+            Plots.plot!(plt_vec[m_pos],x_vec,get_pade(p_u,pade[1],pade[2]).(u_vec),color=:purple,alpha=0.6,linestyle=linestyle_vec[pade_pos],label=label)
         end
     end
 
     ### plot QMC data
     for di in di_vec, (m_pos,m) in enumerate(m_vec)
-        scatter!(plt_vec[m_pos],x_vec_QMC,TGiip_m_QMC[:,1+di,1+m],color=color_vec[di+1],label="",markersize=6)
-        if m==0 annotate!(plt_vec[m_pos],x_vec_QMC[3]+0.5,TGiip_m_QMC[3,1+di,1+m]+0.02,text("i-i'="*string(di),7,color_vec[di+1])) end
+        Plots.scatter!(plt_vec[m_pos],x_vec_QMC,TGiip_m_QMC[:,1+di,1+m],color=color_vec[di+1],label="",markersize=6)
+        if m==0 Plots.annotate!(plt_vec[m_pos],x_vec_QMC[3]+0.5,TGiip_m_QMC[3,1+di,1+m]+0.02,Plots.text("i-i'="*string(di),7,color_vec[di+1])) end
     end
 
     ### integrated-differential approximants
     if false
+        using DifferentialEquations
         for di in di_vec, (m_pos,m) in enumerate(m_vec[1])
 
             p = get_TGiip_m_bare(c_iipDyn_mat,m,12)[center_sites[1]+di,1]
@@ -116,10 +115,11 @@ if true
 
     ### put panels together
     xPlots,yPlots=1,3
-    plt_final = plot(plt_vec...,  layout=(yPlots,xPlots), size=(aps_width*xPlots,0.4*aps_width*yPlots), dpi=600)
+    plt_final = Plots.plot(plt_vec[1],plt_vec[2],plt_vec[3],  layout=(yPlots,xPlots), size=(aps_width*xPlots,0.4*aps_width*yPlots), dpi=600)
     display(plt_final)
-    savefig(plt_final,"examples/Chain/Chain_S1half_Matsubara_r-space.png")
+    #savefig(plt_final,"examples/Chain/Chain_S1half_Matsubara_r-space.png")
 end
+
 
 ###### plot Matsubara correlator in k-space
 if true
@@ -128,9 +128,9 @@ if true
     x_vec_bare = collect(0:0.01:1.96)
     
     ### prepare panels for m=0,1,2
-    plt_m0=plot(xlims=(0,x_vec[end]),ylims=(0,0.75),xformatter=:none,ylabel=L"TG_{k}(i\nu_{m=0})",  label="",legend=:topleft)
-    plt_m1=plot(xlims=(0,x_vec[end]),ylims=(0,0.10),xformatter=:none,ylabel=L"TG_{k}({i\nu_{m=1}})",label="",legend=:topleft)
-    plt_m2=plot(xlims=(0,x_vec[end]),ylims=(0,0.04),xlabel=L"x=J/T",ylabel=L"TG_{k}(i\nu_{m=2})",   label="",legend=:topleft)
+    plt_m0=Plots.plot(xlims=(0,x_vec[end]),ylims=(0,0.75),xformatter=:none,ylabel=L"TG_{k}(i\nu_{m=0})",  label="",legend=:topleft)
+    plt_m1=Plots.plot(xlims=(0,x_vec[end]),ylims=(0,0.10),xformatter=:none,ylabel=L"TG_{k}({i\nu_{m=1}})",label="",legend=:topleft)
+    plt_m2=Plots.plot(xlims=(0,x_vec[end]),ylims=(0,0.04),xlabel=L"x=J/T",ylabel=L"TG_{k}(i\nu_{m=2})",   label="",legend=:topleft)
     plt_vec = [plt_m0,plt_m1,plt_m2]
 
     ### plot Dyn-HTE results
@@ -142,7 +142,7 @@ if true
         ### plot bare truncated series
         label=""
         if (koπ_pos==1 && m==0)  label = L"\mathrm{bare \; series} \; n_{max}="*string(n_max) end    
-        plot!(plt_vec[m_pos],x_vec_bare,p_x.(x_vec_bare),label=label,color=:grey,linewidth=0.5)
+        Plots.plot!(plt_vec[m_pos],x_vec_bare,p_x.(x_vec_bare),label=label,color=:grey,linewidth=0.5)
         pade_vec = [[6,6],[5,5]]
 
         ### x-Padé approximants
@@ -170,20 +170,20 @@ if true
         label=""
         for  (pade_pos,pade) in enumerate(pade_vec)
             if koπ_pos==1 && m==2 label="u-Padé "*string(pade)*" (f=$f)" end
-            plot!(plt_vec[m_pos],x_vec,get_pade(p_u,pade[1],pade[2]).(u_vec),color=:purple,alpha=0.6,linestyle=linestyle_vec[pade_pos],label=label)
+            Plots.plot!(plt_vec[m_pos],x_vec,get_pade(p_u,pade[1],pade[2]).(u_vec),color=:purple,alpha=0.6,linestyle=linestyle_vec[pade_pos],label=label)
         end
     end
 
     ### plot QMC data
     for (koπ_pos,koπ) in enumerate(koπ_vec), (m_pos,m) in enumerate(m_vec)
         TGk_m_QMC = [sum([cos(di*koπ*π)*TGiip_m_QMC[x_pos,1+abs(di),1+m] for di in -imax:1:imax]) for x_pos in eachindex(x_vec_QMC)]
-        if !(m!=0 && koπ==0) scatter!(plt_vec[m_pos],x_vec_QMC,TGk_m_QMC,color=color_vec[7+koπ_pos],label="",markersize=6,marker=:square) end
-        if m==0 annotate!(plt_vec[m_pos],x_vec_QMC[2]+0.1,TGk_m_QMC[2]+0.1-(koπ_pos-1)*0.18,text("k/π="*string(koπ),7,color_vec[7+koπ_pos])) end
+        if !(m!=0 && koπ==0) Plots.scatter!(plt_vec[m_pos],x_vec_QMC,TGk_m_QMC,color=color_vec[7+koπ_pos],label="",markersize=6,marker=:square) end
+        if m==0 Plots.annotate!(plt_vec[m_pos],x_vec_QMC[2]+0.1,TGk_m_QMC[2]+0.1-(koπ_pos-1)*0.18,Plots.text("k/π="*string(koπ),7,color_vec[7+koπ_pos])) end
     end
 
     ### put panels together
     xPlots,yPlots=1,3
-    plt_final = plot(plt_vec...,  layout=(yPlots,xPlots), size=(aps_width*xPlots,0.4*aps_width*yPlots), dpi=600)
+    plt_final = Plots.plot(plt_vec...,  layout=(yPlots,xPlots), size=(aps_width*xPlots,0.4*aps_width*yPlots), dpi=600)
     display(plt_final)
-    savefig(plt_final,"examples/Chain/Chain_S1half_Matsubara_k-space.png")
+    #savefig(plt_final,"examples/Chain/Chain_S1half_Matsubara_k-space.png")
 end
